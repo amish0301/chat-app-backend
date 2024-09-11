3e1998a83dd57af0eae44262e6ca6aa52f6e40ec
amish <pithvaamish76@gmail.com>
2024-09-11 20:41:34 +0530

    testing deployment

--- constants/config.js
+++ constants/config.js
@@ -5,8 +5,7 @@ const corsOptions = {
     process.env.CLIENT_URI,
   ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PUT", "DELETE"]
 };
 
 module.exports = { corsOptions };
